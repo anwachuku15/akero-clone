541079555a86cc20372e58199ec0cbac6e5c9d67
anwachuku15 <anwachuku15@gmail.com>
2020-10-01 10:31:09 -0400

    initial commit

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.css";
+import Carousel from "../components/home/Carousel";
+
+const Home = () => {
+  return (
+    <div className="homePage">
+      <Carousel />
+    </div>
+  );
+};
+
+export default Home;
